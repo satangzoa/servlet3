116f25bf6ed2d64103929abc70809396fab95a4a
A <A@DESKTOP-NKSGQVR>
2023-04-06 10:42:51 +0900

    리스너

--- src/main/java/com/oraclejava/project/web/TestListener.java
+++ src/main/java/com/oraclejava/project/web/TestListener.java
@@ -1,20 +1,25 @@
 package com.oraclejava.project.web;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
-@WebListener
+@WebListener //이벤트핸들러
 public class TestListener implements ServletContextListener{
 
+	// context의 의미 => 현재 실행되고 있는 톰캣
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
-		System.out.println("나 태어났어.. 응애응애...");
+		ServletContext context = 
+				sce.getServletContext();//set 설정하다 get 가져오다
+		String email = context.getInitParameter("webmaster");//web.xml에 있는 webmaster
+		context.setAttribute("webmaster", email);
 	}
 
 	@Override
 	public void contextDestroyed(ServletContextEvent sce) {
-		System.out.println("나 죽어... ^^b");
+
 	}
 	
 	
--- src/main/java/com/oraclejava/project/web/TestServlet.java
+++ src/main/java/com/oraclejava/project/web/TestServlet.java
@@ -9,15 +9,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/TestServlet")
+@WebServlet("/TestServlet") //요청 응답 servlet
 public class TestServlet extends HttpServlet{
+	// context의 의미 => 현재 실행되고 있는 톰캣
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/html; charset=utf-8");
 		PrintWriter out = resp.getWriter();
 		out.println("<html><head><title>TestServlet</title></head>");
-		out.println("<body><h1>Test 중입니다</h1></body>");
+		out.println("<body>");
+		out.println("<h2>Test 중입니다</h2>");	
+		ServletContext context = getServletContext();
+		String webmaster = (String)context.getAttribute("webmaster");
+		out.println("<footer><a href='mailto:" + webmaster + " '>"
+				+ webmaster + "</a></footer>");
+		out.println("</body>");
 		out.println("</html>");
 	
 	}
